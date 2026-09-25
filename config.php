e7b45a4326f9de3b7af1d0dad07824b3e187fc70
deniapriliana10-hub <deniapriliana10@gmail.com>
2026-09-25 12:28:35 +0700

    Update config.php

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 // GANTI dengan API Key Adsterra Anda.
 // Jangan bagikan file config.php ke orang lain.
 return [
-    'api_key' => 'MASUKKAN_API_KEY_ADSTERRA_DISINI',
+    'api_key' => 'fd890e71f1c098760d5166a58f2cf098',
     'api_base' => 'https://api3.adsterratools.com/publisher'
 ];
