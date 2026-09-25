1b5cf1c27f33c3fddd641578c0e18df5e251e3bc
deniapriliana10-hub <deniapriliana10@gmail.com>
2026-09-25 12:26:34 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// GANTI dengan API Key Adsterra Anda.
+// Jangan bagikan file config.php ke orang lain.
+return [
+    'api_key' => 'MASUKKAN_API_KEY_ADSTERRA_DISINI',
+    'api_base' => 'https://api3.adsterratools.com/publisher'
+];
