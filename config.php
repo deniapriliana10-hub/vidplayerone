000218221bc2bcd5002b101dbf5a2cffe604b76e
deniapriliana10-hub <deniapriliana10@gmail.com>
2026-09-25 12:31:50 +0700

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// GANTI dengan API Key Adsterra Anda.
-// Jangan bagikan file config.php ke orang lain.
-return [
-    'api_key' => 'fd890e71f1c098760d5166a58f2cf098',
-    'api_base' => 'https://api3.adsterratools.com/publisher'
-];
